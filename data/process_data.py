a91a389ec079a10aeeab2335d19e95aab7063349
Maxim Kiesel <maxim.kiesel@yahoo.com>
2023-02-22 22:47:32 +0100

    final train_classsifier.py
    + new modul 'text_length_extractor'

--- data/process_data.py
+++ data/process_data.py
@@ -1,73 +1,126 @@
 # import libraries
 import pandas as pd
-import numpy as np
 from sqlalchemy import create_engine
-import nltk
-import ssl
+import sys
 
-# deactivate certificate 
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-# download nltk data 
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
 
 # load data
-df1 = pd.read_csv('/Users/maximkiesel/NLP_Pipline_disaster_response/data/disaster_messages.csv')
-df2 = pd.read_csv('/Users/maximkiesel/NLP_Pipline_disaster_response/data/disaster_categories.csv')
-
-# cleaning the row 'genre'
-df1 = df1.replace(r"\\", "", regex=True)
-
-# cleaning the column name
-df1.rename(columns={'genre\\': 'genre'}, inplace=True)
-df2.rename(columns={'categories\\': 'categories'}, inplace=True)
-
-# delete the last empty row. this empy row breaks the tranforming code of the target  columns
-df2 = df2.drop(26248)
-df1 = df1.drop(26248)
-
-# delete not necessary columns
-df1.drop(columns=['original', 'genre', 'id'], inplace=True)
-
-# get the column name of the target variable
-list_col_target = [] # list for the target columns
-for i in df2.iloc[0,1].split(';'): # loop through the category values
-    col_name = i.split('-')[0]
-    list_col_target.append(col_name)
+def load_data(messages_filepath, categories_filepath):
+    '''
+    Load the csv data
+        
+    INPUT 
+    messages_filepath - csv data with short text messages
+    categories_filepath - csv data with the categorization of the data
+        
+    OUTPUT
+    df1 - dataframe of the short text messages
+    df2 - dataframe of the categorization of the data
+    '''
+    df1 = pd.read_csv(messages_filepath)
+    df2 = pd.read_csv(categories_filepath)
+    return df1, df2
     
-# create a dummy dataframe for the target variable
-y = pd.DataFrame(columns=list_col_target)
+# clean the data
+def clean_data(df1, df2):
+    '''
+    Cleaning both dataframes and unit them to one
+        
+    INPUT 
+    df1 - dataframe of the short text messages
+    df2 - dataframe of the categorization of the data
+        
+    OUTPUT
+    df - united dataframe 
+    '''
+    # cleaning the row 'genre'
+    df1 = df1.replace(r"\\", "", regex=True)
 
-# dummy dictionary for the rows for y
-dummy_df = pd.DataFrame([{name: 0 for name in list_col_target}])
+    # cleaning the column name
+    df1.rename(columns={'genre\\': 'genre'}, inplace=True)
+    df2.rename(columns={'categories\\': 'categories'}, inplace=True)  
+    
+    # delete the last empty row. this empy row breaks the tranforming code of the target  columns
+    df2 = df2.drop(26248)
+    df1 = df1.drop(26248)
 
-# row counter
-row_count = -1
+    # delete not necessary columns
+    df1.drop(columns=['original', 'genre', 'id'], inplace=True)
 
-# fill the y dataframe
-for i in df2['categories'].values:
-    row_count += 1
-    y = pd.concat([y,dummy_df], ignore_index=True)
-    split1 = i.split(';')
-    for j in split1:
-        split2 = j.split('-')
-        if '1' in split2:
-            y.at[row_count, split2[0]] = split2[1]
+    # get the column name of the target variables y
+    list_col_target = [] # list for the target columns
+    for i in df2.iloc[0,1].split(';'): # loop through the category values
+        col_name = i.split('-')[0]
+        list_col_target.append(col_name)
+    
+    # create a dummy dataframe for the target variable
+    y = pd.DataFrame(columns=list_col_target)
+
+    # dummy dictionary for the rows for y
+    dummy_df = pd.DataFrame([{name: 0 for name in list_col_target}])
+
+    # row counter
+    row_count = -1
+
+    # fill the y dataframe
+    for i in df2['categories'].values:
+        row_count += 1
+        y = pd.concat([y,dummy_df], ignore_index=True)
+        split1 = i.split(';')
+        for j in split1:
+            split2 = j.split('-')
+            if '1' in split2:
+                y.at[row_count, split2[0]] = split2[1]
             
 
-# connect both dfs togther 
-concat_df = pd.concat([df1, y], axis=1)
+    # connect both dfs togther 
+    df = pd.concat([df1, y], axis=1)
 
-# delete dulicates
-concat_df.drop_duplicates(inplace=True)
+    # delete duplicates
+    df.drop_duplicates(inplace=True)
+    
+    return df
 
 # create a sql database
-engine = create_engine('sqlite:///cleaned_data_sql.db')
-concat_df.to_sql('clean_disaster_messages', engine, index=False)
+def save_data(df, database_filepath):
+    '''
+    Save the dataframe in a sql database
+        
+    INPUT 
+    df - united dataframe
+    database_filepath - saving path
+        
+    OUTPUT
+    None 
+    '''
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df.to_sql('cleaned_data', engine, index=False)
+    return None
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'.format(messages_filepath, categories_filepath))
+        df1, df2 = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df1, df2)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,23 +1,27 @@
 # load libraries
+import sys
+
 from sqlalchemy import create_engine, text
 import ssl
 
 import pandas as pd
-import numpy as np
+import pickle
 
 import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
-from nltk.tokenize import RegexpTokenizer, word_tokenize
+from nltk.tokenize import word_tokenize
 
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split, ShuffleSplit, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+
+from text_length_extractor import TextLengthExtractor
+
 
 # deactivate ssl certificate to download the nltk package
 try:
@@ -33,16 +37,25 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('wordnet')
 
-
-def load_data():
+def load_data(database_filepath):
+    '''
+    Loading the data from a sql database and transform it to X, y data for the machine learning model.
+    
+    INPUT 
+    database_filepath - path to the sql database
+      
+    OUTPUT
+    X - Features
+    y - Target variables
+    '''
     # create the engine for the sql database
-    engine = create_engine('sqlite:///cleaned_data_sql.db')
+    engine = create_engine(f'sqlite:///{database_filepath}')
     
     # create a connection
     conn = engine.connect()
     
     # transform to a executable object for pandas
-    sql = text("SELECT * FROM clean_disaster_messages")
+    sql = text("SELECT * FROM cleaned_data")
     
     # create the dataframe
     df = pd.read_sql(sql, conn)
@@ -50,6 +63,7 @@ def load_data():
     # data split
     X = df['message']
     y = df.drop(columns=['message'])
+    y = y.astype('int')
     
     # delete number-only rows 
     row_count = -1
@@ -59,32 +73,187 @@ def load_data():
             X.drop(row_count)
     return X, y
 
-# displaying the ml performance
-def display_results(y_test, y_pred):
-    labels = np.unique(y_pred)
-    confusion_mat = confusion_matrix(y_test['related'], y_pred['related'], labels=labels)
-    accuracy = (y_pred == y_test).mean()
 
-    print("Labels:", labels)
-    print("Confusion Matrix:\n", confusion_mat)
-    print("Accuracy:", accuracy)
- 
-# machine learning pipline   
-def main():
-    X, y = load_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+def tokenize(message):
+    '''
+    Cleaning, lower letters, tokenize, remove stopwords and lemmatize on the base of part-of-speech.
+    
+    INPUT 
+    message - single message of the corpus
+      
+    OUTPUT
+    clean_message - single, cleaned message of the corpus
+    '''
+    # remove url´s from message
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    message = url_pattern.sub(' ', message)
+    
+    # remove all symboles which are not letter or numbers
+    sym_pattern = re.compile(r'[^a-zA-Z0-9]')
+    message = sym_pattern.sub(' ', message)
+    
+    # transfrom to lower case
+    message = message.lower()
+    
+    # tokenize message
+    message = word_tokenize(message)
+    
+    # apply part of speech tagging
+    message = pos_tag(message)
+    
+    # remove stop words
+    stop_words = set(stopwords.words("english"))
+    message = [word for word in message if word[0] not in stop_words]
+    
+    # find the right index for the root word in the WordNetLemmatizer().lemmatize
+    def pos_var(word):
+        pos_value = 0
+        if word[1].startswith('J'):
+            pos_value = 'a'
+        elif word[1].startswith('V'):
+            pos_value = 'v'
+        elif word[1].startswith('N'):
+            pos_value = 'n'
+        elif word[1].startswith('R'):
+            pos_value = 'r'
+        else:
+            pos_value = 'n'
+        return pos_value
+    
+    # lemmatize function for the word in the word-tag-pair
+    word_tag_count = -1
+    for word_tag in message:
+        word_tag_count += 1
+        message[word_tag_count] = WordNetLemmatizer().lemmatize(word_tag[0], pos=pos_var(word_tag))
+    
+    clean_message = message
+    
+    return clean_message
 
+
+def build_model():
+    '''
+    Machine learning pipelinie for an NLP use case which use Count-Vectorizer, TF-IDF and 
+    Text Length Extracting. After that it finds with Gridsearch the best
+    parameters for a RandomForest Classifier.
+    '''
     pipeline = Pipeline([
-        ('vect', CountVectorizer(tokenizer=tokenize)),
-        ('tfidf', TfidfTransformer()),
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('txt_len', TextLengthExtractor())
+        ])),
+
         ('clf', RandomForestClassifier())
     ])
+    
+    # define the split for gridsearch
+    shuffle = ShuffleSplit(n_splits=5, test_size=0.3, random_state=42)
+    
+    # define set of hyperparameters for test of the ml model with gridsearch
 
-    # train classifier
-    pipeline.fit(X_train, y_train)
+    parameters = {
+        'clf__max_depth': [400, 600],
+        'clf__n_estimators': [800, 1000]
+    }
+    
+    model = GridSearchCV(pipeline, param_grid=parameters, cv=shuffle)
 
-    # predict on test data
-    y_pred = pipeline.predict(X_test)
+    return model
 
-    # display results
-    display_results(y_test, y_pred)
+
+
+def evaluate_model(model, X_test, y_test):
+    '''
+    Cleaning, lower letters, tokenize, remove stopwords and lemmatize on the base of part-of-speech.
+    
+    INPUT 
+    model - machine Learning Pipeline in a NLP Use Case
+    X_test - splitted test features 
+    y_test - splitted test, target variables
+      
+    OUTPUT
+    None
+    '''
+    target_names = list(y_test.columns)
+    y_pred = model.predict(X_test)
+    report = classification_report(y_test, y_pred, target_names=target_names)
+    print("Labels:", target_names)
+    print("Classification Report:\n", report)
+    print("\nBest Parameters:", model.best_params_)
+
+
+def save_model(model, model_filepath):
+    '''
+    Transform and save the trained model in a pickle file.
+    
+    INPUT 
+    model - machine Learning Pipeline in a NLP Use Case
+    model_filepath - path to the file
+    
+    OUTPUT
+    None
+    '''
+    # save the model as a pickle file
+    with open(model_filepath, 'wb') as model:
+        pickle.dump(model, model)
+
+
+def main():
+    '''
+    Main function to start the pipeline process.
+    '''
+    
+    if len(sys.argv) == 3: # Check if there are 3 inputs
+        
+        # assign the variables 
+        database_filepath, model_filepath = sys.argv[1:]
+       
+        # print a loading status
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        
+        # assign the X and y variables
+        X, y = load_data(database_filepath)
+        
+        # splitting in train and test data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+        
+        # print a building status
+        print('Building model...')
+        
+        # definie model
+        model = build_model()
+        
+        # print a training status
+        print('Training model...')
+        
+        # train the model
+        model.fit(X_train, y_train)
+        
+        # print a evaluating status
+        print('Evaluating model...')
+        
+        # evaluate the model
+        evaluate_model(model, X_test, y_test)
+
+        # print a saving status
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        
+        # save the model as a pickle file
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
